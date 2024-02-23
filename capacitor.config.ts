582654a02cc665824081ddcf298ce45d59981dc5
Katerin Sanz Celorrio <ksanz@birt.eus>
2024-02-23 10:38:00 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'DI04_TareaEvaluativa_Dashboard',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
